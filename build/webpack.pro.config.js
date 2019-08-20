a6b568ef654cc2d1656a72f52d90c5013a57582e
luzhenqian <15753140326@163.com>
2019-08-21 01:14:51 +0800

    style: gitignore add /dist

--- build/webpack.pro.config.js
+++ build/webpack.pro.config.js
@@ -3,5 +3,6 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
   plugins: [
     new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ["./dist"] })
-  ]
+  ],
+  devtool: "cheap-module-eval-source-map"
 };
