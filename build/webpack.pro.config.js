9eef34098eebf17fb8776a0eba2ae6959c6f6c75
luzhenqian <15753140326@163.com>
2019-08-21 01:16:58 +0800

    fix: delete webpack.pro devtool

--- build/webpack.pro.config.js
+++ build/webpack.pro.config.js
@@ -3,6 +3,5 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
   plugins: [
     new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ["./dist"] })
-  ],
-  devtool: "cheap-module-eval-source-map"
+  ]
 };
